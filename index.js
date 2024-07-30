a2cf237d0f01626aaa84015c63d0d6b45e366dcc
rezha4 <rezha.bahari@gmail.com>
2024-07-30 13:34:13 +0700

    added /premiumnanny

--- index.js
+++ index.js
@@ -39,6 +39,39 @@ app.get("/blog", async (req, res) => {
   }
 });
 
+app.get('/premiumnanny', async (req, res) => {
+  try {
+    // Fetch the product data from the WordPress API
+    const productResponse = await fetch('https://find.premiumnanny.id/wp-json/wp/v2/product');
+    const products = await productResponse.json();
+
+    // Extract the title, featured image ID, and other necessary details
+    const result = await Promise.all(products.map(async (product) => {
+      const { id, title, featured_media, link } = product;
+
+      // Fetch the media details to get the image URL
+      let imageUrl = '';
+      if (featured_media) {
+        const mediaResponse = await fetch(`https://find.premiumnanny.id/wp-json/wp/v2/media/${featured_media}`);
+        const media = await mediaResponse.json();
+        imageUrl = media.guid.rendered;
+      }
+
+      return {
+        id,
+        title: title.rendered,
+        image: imageUrl,
+        link: link
+      };
+    }));
+
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 app.listen(port, () => {
   console.log("app listening on port", port);
 });
